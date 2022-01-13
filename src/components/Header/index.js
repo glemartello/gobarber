7b07e6d440a061a9aa26474c409bcbae38409a9d
Glenda Martello <glemartello@hotmail.com>
2022-01-13 17:14:59 -0300

    stylizing dashboard

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Notifications from '~/components/Notifications';
@@ -8,6 +9,8 @@ import logo from '~/assets/logo.svg';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+    const profile = useSelector((state) => state.user.profile);
+
     return (
         <Container>
             <Content>
@@ -20,11 +23,14 @@ export default function Header() {
 
                     <Profile>
                         <div>
-                            <strong>Glenda Martello</strong>
+                            <strong>{profile.name}</strong>
                             <Link to="/profile">Meu perfil</Link>
                         </div>
                         <img
-                            src="https://avatars.dicebear.com/api/big-smile/glendamartello.svg"
+                            src={
+                                profile.avatar.url ||
+                                'https://avatars.dicebear.com/api/big-smile/glendamartello.svg'
+                            }
                             alt="Glenda Martello"
                         />
                     </Profile>
